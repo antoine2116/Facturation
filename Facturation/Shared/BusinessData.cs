267a611d82726a7841eeed2033256c8c2f4547d4
Antoine <antooinerobin@gmail.com>
2020-12-07 17:10:29 +0100

    Ajoutez des fichiers projet.

---
+++ Facturation/Shared/BusinessData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facturation.Shared
+{
+    public class BusinessData : IBusinessData
+    {
+        public List<Facture> lesFactures { get; } = new List<Facture>();
+
+        public BusinessData(params Facture[] factures)
+        {
+            lesFactures.AddRange(factures);
+        }
+    }
+}
